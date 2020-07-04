13e9593027d35688ad7949d169b26ed4c549909f
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2020-07-03 22:22:19 -0300

    Storing accounts into database.

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    "OPERATORSaLIASES": "0",
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    operatorAliases: "0",
   },
   "test": {
     "username": "root",
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
+
 const router = express.Router();
 
-router.get('/sign-in', (req, res) => res.json('Sign In!'));
+const saltRounds = 10;
 
-router.get('/sign-up', (req, res) => res.json('Sign Up!'));
+router.get('/sign-in', (req, res) => {
+    return res.json('Sign In!');
+});
+
+router.get('/sign-up', async (req, res) => {
+
+    const email = 'adriano.cirino.da.silva@gmail.com';
+    const password = '98550754aa';
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+    const result = await Account.create({ email, password: hash });
+    
+    return res.json(result);
+});
 
 module.exports = router;
